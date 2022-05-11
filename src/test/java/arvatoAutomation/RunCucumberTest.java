51a528199fe56f9aea8e796bbe0027d882b1846c
Gulkhara Babayeva <gulkharababayeva@Gulkharas-MacBook-Air.local>
2022-05-11 19:19:14 +0300

    fix allure

--- src/test/java/arvatoAutomation/RunCucumberTest.java
+++ src/test/java/arvatoAutomation/RunCucumberTest.java
@@ -7,10 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/arvatoAutomation.features",
-        glue = {"arvatoAutomation.stepDefinitions"}
-//        plugin = {
-//                "json",
-//                "json:target/cucumber.json"}
+        glue = {"arvatoAutomation.stepDefinitions"},
+        plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class RunCucumberTest {
 }
